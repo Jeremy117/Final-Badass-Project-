4b0c6950cc8dc9762306927252013b620ef64526
alvaro cortes <frijoles4me@gmail.com>
2018-03-14 09:43:52 -0700

    frontpage and login

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+//import logo from "./logo.svg";
+import "./App.css";
+import Login from "./Login";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <div>
+            <Link to="/Login">Login</Link>
+          </div>
+          <p>
+            <h1>HUDDLE</h1>
+            <h2 /> MANAGE YOUR SPORTS TEAM
+            <div />The sports team management application
+            <div />for coaches, parents and players.
+          </p>
+          <Route path="/Login" component={Login} />
+        </div>
+      </Router>
     );
   }
 }
