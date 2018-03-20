44dd23402794d48536b0f7cef8bd78061c59f3bf
JENNIFER ANDRADE <jen43077@yahoo.com>
2018-03-20 10:50:26 -0700

    Updated SignUp and Login

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 // import "./App.css";
 import React, { Component } from "react";
 import Home from "./Pages/Home";
-import Login from "./Pages/Login";
+import signUp from "./Pages/Login/signUp";
 import About from "./Pages/About";
+import Register from "./Pages/Login/Register";
 
 class App extends Component {
   render() {
@@ -17,18 +18,22 @@ class App extends Component {
                 <Link to="/">Home</Link>
               </div>
               <div>
-                <Link to="/Pages/Login">Login</Link>
+                <Link to="/Pages/Login/signUp">SignUp</Link>
               </div>
               <div>
                 <Link to="/Pages/About">About</Link>
+              </div>
+              <div>
+                <Link to="/Pages/Login/Register">Register</Link>
               </div>
             </ul>
           </nav>
           <div>
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/Pages/Login" component={Login} />
+              <Route path="/Pages/Login/signUp" component={signUp} />
               <Route path="/Pages/About" component={About} />
+              <Route path="/Pages/Login/Register" component={Register} />
             </Switch>
           </div>
         </div>
---
+++ client-app/src/Pages/Login/Register.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { format } from "util";
+
+class register extends Component {
+    render() {
+        return (
+            <form className="signUp">
+                <div className="register">
+                    <h1 /> Register
+          <div>
+                        <label for="First Name">First Name:</label>
+                        <input
+                            name="First"
+                            placeholder="John"
+                            id="First Name"
+                            type="text"
+                            required
+                        />
+
+                        <label for="Last Name">Last Name:</label>
+                        <input
+                            name="Last"
+                            placeholder="Smith"
+                            id="Last Name"
+                            type="text"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label for="Email">Email:</label>
+                        <input
+                            placeholder="your email"
+                            id="Email"
+                            type="Email"
+                            name="email"
+                            required
+                        />
+
+                        <label for="Password">Password</label>
+                        <input
+                            placeholder="your password"
+                            id="Password"
+                            type="Password"
+                            maxlength="10"
+                            minlength="5"
+                            name="password"
+                            required
+                        />
+                    </div>
+                    <div>
+                        <input type="submit" />
+                    </div>
+                </div>
+            </form>
+        );
+    }
+}
+export default register;
