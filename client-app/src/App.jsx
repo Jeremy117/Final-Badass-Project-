648bcfe8fb7d36b52df9035e979bc0d2eee51396
alvaro cortes <frijoles4me@gmail.com>
2018-03-22 12:30:21 -0700

    udate

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/CustomNavBar";
+import Navbar from "./components/CustomNavbar";
 import Home from "./components/Home";
 import Register from "./components/Register";
 import signUp from "./components/signUp";
 import Dashboard from "./components/Dashboard";
-
 
 class App extends Component {
   render() {
