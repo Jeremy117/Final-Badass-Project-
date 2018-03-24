7c7be96267b4a3148b1ce21f7ca0d09ef8c9b698
JENNIFER ANDRADE <jen43077@yahoo.com>
2018-03-23 23:01:20 -0700

    added some style to signUp and Register

--- client-app/src/components/Register.jsx
+++ client-app/src/components/Register.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 
 const mapStateToProps = state => ({
@@ -10,52 +12,87 @@ class Register extends Component {
     return (
       <form>
         <div className="register">
-          <h1 /> Register
-        <div>
-            <label for="First Name">First Name:</label>
-            <input
-              name="First"
-              placeholder="John"
-              id="First Name"
-              type="text"
-              required
-            />
-
-            <label for="Last Name">Last Name:</label>
-            <input
-              name="Last"
-              placeholder="Smith"
-              id="Last Name"
-              type="text"
-              required
-            />
+          <div className="row">
+            <div className="col-md-2 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Register</h1>
+              <p className="text-xs-center">
+              </p>
+            </div>
           </div>
-          <div>
-            <label for="Email">Email:</label>
-            <input
-              placeholder="your email"
-              id="Email"
-              type="Email"
-              name="email"
-              required
-            />
 
-            <label for="Password">Password</label>
-            <input
-              placeholder="your password"
-              id="Password"
-              type="Password"
-              maxlength="10"
-              minlength="5"
-              name="password"
-              required
-            />
-          </div>
-          <div>
-            <input type="submit" />
+          <div className="form-group">
+            <label for="First Name" className="col-sm-10 col-form-label">First Name</label>
+            <div className="col-sm-5">
+              <input
+                className="form-control form-control-sm"
+                name="First"
+                placeholder="John"
+                id="First Name"
+                type="text"
+                required
+              />
+            </div>
+            <div>
+              <label for="Last Name" className="col-sm-10 col-form-label">Last Name</label>
+              <div className="col-sm-5">
+                <input
+                  className="form-control form-control-sm"
+                  name="Last"
+                  placeholder="Smith"
+                  id="Last Name"
+                  type="text"
+                  required
+                />
+              </div>
+
+              <div>
+                <label for="Email" className="col-sm-10 col-form-label">Email</label>
+                <div className="col-sm-5">
+                  <input
+                    className="form-control form-control-sm"
+                    placeholder="Enter Email"
+                    id="Email"
+                    type="Email"
+                    name="email"
+                    required
+                  />
+                </div>
+                <div>
+                  <label for="Password" className="col-sm-10 col-form-label">Password</label>
+                  <div className="col-sm-5">
+                    <input
+                      className="form-control form-control-sm"
+                      placeholder="Create Password"
+                      id="Password"
+                      type="Password"
+                      maxlength="10"
+                      minlength="5"
+                      name="password"
+                      required
+                    />
+                    <small id="passwordHelpInline" class="text-muted">Must be 5-10 characters long.</small>
+                  </div>
+
+                  <div>
+                    <button
+                      className="btn btn-primary pull-xs-right"
+                      type="submit"
+                      disabled={this.props.inProgress}
+                    >
+                      Register
+                  </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </form>
+
+
+
+
+
     );
   }
 }
--- client-app/src/components/signUp.jsx
+++ client-app/src/components/signUp.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 const mapStateToProps = state => ({
   appName: state.appName
@@ -8,32 +9,56 @@ const mapStateToProps = state => ({
 class signUp extends Component {
   render() {
     return (
-      <form className="signUp">
-        <h1 /> Sign In
+      <form>
         <div className="signIn">
-          <label for="Email">Email:</label>
-          <input
-            placeholder="your email"
-            id="Email"
-            type="Email"
-            name="email"
-            required
-          />
-          <label for="Password">Password</label>
-          <input
-            placeholder="your password"
-            id="Password"
-            type="Password"
-            maxlength="10"
-            minlength="5"
-            name="password"
-            required
-          />
-          <div>
-            <input type="submit" />
+          <div className="row">
+            <div className="col-sm-2 offset-md-3 col-xs-12">
+              <h1 className="text-xs-center">Sign In</h1>
+              <p className="text-xs-center">
+              </p>
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label for="Email" className="col-sm-10 col-form-label">Email address</label>
+            <div className="col-sm-5">
+              <input
+                type="Email"
+                className="form-control"
+                placeholder="Enter email "
+                id="Email"
+                name="email"
+                required
+              />
+              <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+            </div>
+
+            <label for="Password" className="col-sm-10 col-form-label">Password</label>
+            <div className="col-sm-5">
+              <input
+                className="form-control form-control-sm"
+                placeholder="Password"
+                id="Password"
+                type="Password"
+                maxlength="10"
+                minlength="5"
+                name="password"
+                required
+              />
+              <div>
+                <button
+                  className="btn btn-primary pull-xs-right"
+                  type="submit"
+                  disabled={this.props.inProgress}
+                >
+                  Submit
+                  </button>
+              </div>
+            </div>
           </div>
         </div>
       </form>
+
     );
   }
 }
