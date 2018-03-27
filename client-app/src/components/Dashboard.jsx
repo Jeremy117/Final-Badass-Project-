613ec53d7ded0e01187cdcf484f8c4d8e282335c
Thomas Lawson <Thomas@Daniels-MacBook-Pro-2.local>
2018-03-27 14:39:02 -0700

    update

--- client-app/src/components/Dashboard.jsx
+++ client-app/src/components/Dashboard.jsx
@@ -1,6 +1,18 @@
 import React, { Component } from "react";
 import "./Dashboard.css";
 import { Grid, Row, Col } from "react-bootstrap";
+import InfiniteCalendar from "react-infinite-calendar";
+import "react-infinite-calendar/styles.css"; // Make sure to import the default stylesheet
+import { connect } from "react-redux";
+
+// Render the Calendar
+
+var today = new Date();
+var lastWeek = new Date(
+  today.getFullYear(),
+  today.getMonth(),
+  today.getDate() - 7
+);
 
 const arrTeams = [
   "Titian",
@@ -23,55 +35,37 @@ const dummySentences = [
   "Nullam tristique diam non turpis.",
   "Cras placerat accumsan nulla.",
   "Nullam rutrum.",
-  "Nam vestibulum accumsan nisl."
+  "Nam vestibulum accumsan nisl....."
 ];
 
 class Dashboard extends Component {
   render() {
     return (
-      <Grid>
-        <Row className="show-grid">
-          <Col sm={6} md={3} className="container1">
-            <code>&lt;{"Col sm={6} md={3}"} /">">&gt;</code>
-            <br />
-            {dummySentences.slice(0, 6).join(" ")}
-          </Col>
-          <Col sm={6} md={3} className="container1">
-            <code>&lt;{"Col sm={6} md={3}"} /">">&gt;</code>
-            <br />
-            {dummySentences.slice(0, 4).join(" ")}
-          </Col>
-          <Col sm={6} md={3} className="container1">
-            <div>Teams!!{listArr}</div>
-            <br />
-          </Col>
-        </Row>
-      </Grid>
-
-      //   <div class="row">
-      //     <div class="col-md-1">.col-md-1</div>
-      //     <div class="col-md-1">.col-md-1</div>
-      //     <div class="col-md-1">
-      //       <div>Teams!!!</div>
-      //       {listArr}
-      //     </div>
-      //   </div>
-      //   <div>
-      //     <div className="container2">
-      //       <h1 /> Dashboard
-      //     </div>
-      //     <ul className="container1">
-      //       <h2 />calendar
-      //       <h2 />newsfeed
-      //       <h2 />roster
-      //     </ul>
-      //     <ul className="container1">
-      //       <div />calendar code
-      //       <div />newsfeed code
-      //       <div className="container2" />
-      //       {listArr}
-      //     </ul>
-      //   </div>
+      <div>
+        <Grid>
+          <Row className="show-grid">
+            <Col sm={-11116} md={-3} className="container3">
+              <InfiniteCalendar
+                width={269}
+                height={3000}
+                selected={today}
+                disabledDays={[0, 6]}
+                minDate={lastWeek}
+              />,
+              <br />
+            </Col>
+            <Col sm={6} md={3} className="container2">
+              <code>&lt;&gt;</code>
+              <br />
+              {dummySentences.slice(0, 4).join(" ")}
+            </Col>
+            <Col sm={1} md={33} className="container1">
+              <div>Teams!!{listArr}</div>
+              <br />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
     );
   }
 }
