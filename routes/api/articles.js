946bfdc525061d182289fb2132e93e555ef86f60
Adam Klaasmeyer <adamklaasmeyer@gmail.com>
2018-03-29 09:55:08 -0700

    changed port to 3001 inpackage.json

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const mongoose = require("mongoose");
 const Article = mongoose.model("Article");
 
-router.get("/", function(req, res, next) {
+router.get("/article", function(req, res, next) {
   //this is where we'd query db, give all the articles
   // get the results
   //and send them back
