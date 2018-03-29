fca19b1234d374af939db306584c80a7f7ec7b96
Adam Klaasmeyer <adamklaasmeyer@gmail.com>
2018-03-28 18:38:01 -0700

    restoring v3

--- client-app/src/index.js
+++ client-app/src/index.js
@@ -1,26 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-// import { BrowserRouter } from "react-browser-router";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import store from "./store";
+
 import registerServiceWorker from "./registerServiceWorker";
 
-const defaultState = {
-  appName: "technology-notes",
-  articles: null
-};
-
-const reducer = function(state = defaultState, action) {
-  return state;
-};
-
-const reduxStore = createStore(reducer);
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
 
+//second version
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import { createStore } from "redux";
+// import { Provider } from "react-redux";
+// // import { BrowserRouter } from "react-browser-router";
+// import "./index.css";
+// import App from "./App";
+// import registerServiceWorker from "./registerServiceWorker";
+
+// const defaultState = {
+//   appName: "technology-notes",
+//   articles: null
+// };
+
+// const reducer = function(state = defaultState, action) {
+//   return state;
+// };
+
+// const reduxStore = createStore(reducer);
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+// registerServiceWorker();
+
+//first version
 // ReactDOM.render(
 //   <Provider store={reduxStore}>
 //     <BrowserRouter>
