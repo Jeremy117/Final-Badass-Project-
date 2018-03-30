1b5914ce0b39ad3e73c0b30c8a5bd330226b94ef
JENNIFER ANDRADE <jen43077@yahoo.com>
2018-03-30 10:32:34 -0700

    Update Profile

--- client-app/src/components/Profile.jsx
+++ client-app/src/components/Profile.jsx
@@ -1,168 +1,181 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "./Profile.css";
 
 
 class Profile extends Component {
-    state = {
-        image: "",
-        username: "",
-        bio: "",
-        email: "",
-        password: ""
-    };
-    //On Mount we want to assign all the currentUser props to the forms state,
-    //so it's filled out
-    componentDidMount() {
-        if (this.props.currentUser) {
-            this.setState({
-                ...this.state,
-                ...this.props.currentUser
-            });
-        }
-    }
+    // state = {
+    //     image: "",
+    //     username: "",
+    //     bio: "",
+    //     email: "",
+    //     password: ""
+    // };
+    // //On Mount we want to assign all the currentUser props to the forms state,
+    // //so it's filled out
+    // componentDidMount() {
+    //     if (this.props.currentUser) {
+    //         this.setState({
+    //             ...this.state,
+    //             ...this.props.currentUser
+    //         });
+    //     }
+    // }
 
-    //we also need the form to update after possible rehydration, or changes to currentUser
-    componentWillReceiveProps(nextProps) {
-        if (nextProps.currentUser) {
-            this.setState({
-                ...this.state,
-                image: nextProps.currentUser.image || "",
-                username: nextProps.currentUser.username,
-                bio: nextProps.currentUser.bio,
-                email: nextProps.currentUser.email
-            });
-        }
-    }
+    // //we also need the form to update after possible rehydration, or changes to currentUser
+    // componentWillReceiveProps(nextProps) {
+    //     if (nextProps.currentUser) {
+    //         this.setState({
+    //             ...this.state,
+    //             image: nextProps.currentUser.image || "",
+    //             username: nextProps.currentUser.username,
+    //             bio: nextProps.currentUser.bio,
+    //             email: nextProps.currentUser.email
+    //         });
+    //     }
+    // }
 
-    submitForm = event => {
-        event.preventDefault();
+    // submitForm = event => {
+    //     event.preventDefault();
 
-        const user = Object.assign({}, this.state);
-        //we don't want to HTTP PUT a blank user
-        if (!user.password) {
-            delete user.password;
-        }
+    //     const user = Object.assign({}, this.state);
+    //     //we don't want to HTTP PUT a blank user
+    //     if (!user.password) {
+    //         delete user.password;
+    //     }
 
-        this.props.onSubmitForm(user);
-    };
+    //     this.props.onSubmitForm(user);
+    // };
 
-    updateState = field => event => {
-        const state = this.state;
-        const newState = Object.assign({}, state, { [field]: event.target.value });
-        this.setState(newState);
-    };
+    // updateState = field => event => {
+    //     const state = this.state;
+    //     const newState = Object.assign({}, state, { [field]: event.target.value });
+    //     this.setState(newState);
+    // };
     render() {
         return (
-            <div className="container">
-
-
-                <div className="row extra-margin">
-
-                    <div className="col-sm-10">
+            <form>
+                <div className="container">
+                    <div className="row">
 
 
 
-                        <img src="http://via.placeholder.com/400x300" />
+                        <div className="col-md-7 ">
 
-                        <h2>Jennifer Andrade</h2>
+                            <div className="panel panel-default">
+                                <div className="panel-heading">  <h4 >User Profile</h4></div>
+                                <div className="panel-body">
+
+                                    <div className="box box-info">
+
+                                        <div className="box-body">
+                                            <div className="col-sm-5">
+                                                <div align="center"> <img alt="User Pic" src="https://x1.xingassets.com/assets/frontend_minified/img/users/nobody_m.original.jpg" id="profile-image1" className="img-circle img-responsive" />
+
+                                                    <input id="profile-image-upload" className="hidden" type="file" />
+                                                    <button className="btn btn-primary" type="button" id="custom-button">Edit Photo</button>
+
+
+
+                                                </div>
+
+                                                <br />
+
+                                                {/* <!-- /input-group --> */}
+                                            </div>
+                                            <div className="col-sm-6">
+                                                <h4 styles="color:#00b1b1;">Jennifer Andrade</h4>
+                                                <span><p>Catcher</p></span>
+                                            </div>
+                                            <div className="clearfix"></div>
+                                            <hr styles="margin:5px 0 5px 0;" />
+
+
+                                            <div className="col-sm-5 col-xs-6 tital " >First Name:</div><div className="col-sm-7 col-xs-6 "><input type="text" placeholder="First Name"></input></div>
+                                            <div className="clearfix"></div>
+                                            <div className="bot-border"></div>
+
+                                            <div className="col-sm-5 col-xs-6 tital " >Last Name:</div><div className="col-sm-7"><input type="text" placeholder="Last Name"></input></div>
+                                            <div className="clearfix"></div>
+                                            <div className="bot-border"></div>
+
+                                            <div className="col-sm-5 col-xs-6 tital " >Email Address:</div><div className="col-sm-7"><input type="text" placeholder="Email Adress"></input></div>
+                                            <div className="clearfix"></div>
+                                            <div className="bot-border"></div>
+
+                                            <div className="col-sm-5 col-xs-6 tital " >Password:</div><div className="col-sm-7"><input type="text" placeholder="Password"></input></div>
+
+                                            <div className="clearfix"></div>
+                                            <div className="bot-border"></div>
+
+                                            <div className="col-sm-5 col-xs-6 tital " >Position:</div><div className="col-sm-7"><input type="text" placeholder="Position"></input></div>
+
+                                            <div className="clearfix"></div>
+                                            <div className="bot-border"></div>
+
+                                            <div className="col-sm-5 col-xs-6 tital " >Phone Number:</div><div className="col-sm-7"><input type="text" placeholder="Phone Number"></input></div>
+
+                                            <div className="clearfix"></div>
+                                            <div className="bot-border"></div>
+
+
+
+
+                                            <button
+                                                className="btn btn-primary pull-xs-right"
+                                                type="submit"
+                                                disabled={this.props.inProgress}
+                                            >
+                                                Edit Info
+                  </button>
+
+                                            <button
+                                                className="btn btn-primary pull-xs-right"
+                                                type="submit"
+                                                disabled={this.props.inProgress}
+                                            >
+                                                Save
+                  </button>
+
+
+                                            {/* <!-- /.box-body --> */}
+                                        </div>
+                                        {/* <!-- /.box --> */}
+
+                                    </div>
+
+
+                                </div>
+                            </div>
+                        </div>
+                        <script>
+                            {function () {
+                                ('#profile-image1').on('click', function () {
+                                    ('#profile-image-upload').click();
+                                });
+                            }};
+              </script>
+
+
+
+
+
+
+
 
 
 
 
                     </div>
-
-
                 </div>
-
-
-
-                <form onSubmit={this.submitForm}>
-
-                    <fieldset>
-                        <fieldset className="form-group">
+            </form>
 
 
 
 
 
-                            <div className="col-sm-10 col-md-8 col-form-label"></div>
-                            <div className="col-sm-5">
 
 
-                                <input
-                                    className="form-control"
-                                    type="text"
-                                    placeholder="URL of profile picture"
-                                    value={this.state.image}
-                                    onChange={this.updateState("image")}
-                                />
-                            </div>
-                        </fieldset>
-
-                        <fieldset className="form-group">
-                            <div className="col-sm-10 col-form-label"></div>
-                            <div className="col-sm-5">
-                                <input
-                                    className="form-control form-control-lg"
-                                    type="text"
-                                    placeholder="Username"
-                                    value={this.state.username}
-                                    onChange={this.updateState("username")}
-                                />
-                            </div>
-                        </fieldset>
-
-                        <fieldset className="form-group">
-                            <div className="col-sm-10 col-form-label"></div>
-                            <div className="col-sm-5">
-                                <textarea
-                                    className="form-control form-control-lg"
-                                    rows="8"
-                                    placeholder="Short bio about you"
-                                    value={this.state.bio}
-                                    onChange={this.updateState("bio")}
-                                />
-                            </div>
-                        </fieldset>
-
-                        <fieldset className="form-group">
-                            <div className="col-sm-10 col-form-label"></div>
-                            <div className="col-sm-5">
-                                <input
-                                    className="form-control form-control-lg"
-                                    type="email"
-                                    placeholder="Email"
-                                    value={this.state.email}
-                                    onChange={this.updateState("email")}
-                                />
-                            </div>
-                        </fieldset>
-
-                        <fieldset className="form-group">
-                            <div className="col-sm-10 col-form-label"></div>
-                            <div className="col-sm-5">
-                                <input
-                                    className="form-control form-control-lg"
-                                    type="password"
-                                    placeholder="New Password"
-                                    value={this.state.password}
-                                    onChange={this.updateState("password")}
-                                />
-                            </div>
-                        </fieldset>
-
-                        <button
-                            className="btn btn-lg btn-primary pull-xs-right"
-                            type="submit"
-                            disabled={this.state.inProgress}
-                        >
-                            Update Settings
-          </button>
-                    </fieldset>
-                </form>
-
-
-            </div>
 
 
 
