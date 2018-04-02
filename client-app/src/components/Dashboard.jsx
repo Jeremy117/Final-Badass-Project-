231c2b2ddeae6f0d6b04657b3aae2e9ef6e9519b
alvaro cortes <frijoles4me@gmail.com>
2018-04-02 16:38:58 -0700

    updates to articles

--- client-app/src/components/Dashboard.jsx
+++ client-app/src/components/Dashboard.jsx
@@ -1,31 +1,45 @@
 import React, { Component } from "react";
 import "./Dashboard.css";
+import axios from "axios";
 
+const URL =
+  "https://api.mlab.com/api/1/databases/heroku_57qw8z8r/collections/articles?apiKey=h-OMydwAhmajzJr_hWshGs0gjrPxVKKa";
 
 class Dashboard extends Component {
-    render() {
-        return (
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      show: false,
+      articles: []
+    };
+  }
+  getRequest = () => {
+    axios.get(URL).then(res => {
+      this.setState({
+        show: true,
+        articles: res.data
+      });
+      // console.log(res.data);
+      // console.log(response.status);
+      // console.log(response.statusText);
+      // console.log(response.headers);
+    });
+  };
+  render() {
+    return (
+      <div>
+        <div className="container2">
+          <div>
+            <button onClick={this.getRequest}>Get Newsfeed!</button>
             <div>
-                <div className="container2">
-                    <h1 /> Dashboard
+              {this.state.articles.map(article => <li>{article.title}</li>)}
             </div>
-                <ul className="container1">
-                    <h2 />calendar
-              <h2 />newsfeed
-              <h2 />roster
-            </ul>
-                <ul className="container1">
-                    <div />calendar code
-              <div />newsfeed code
-              <div />roster code
-            </ul>
-            </div>
-
-        );
-
-    }
-
-
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
