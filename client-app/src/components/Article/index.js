fb064c53db4fa3255093fdc92c124d7110129bb0
Kmakj <jacob.kmak@gmail.com>
2018-04-02 13:46:40 -0700

    Initialization

---
+++ client-app/src/components/Article/index.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import MainView from "./MainView";
+import services from "../../services";
+
+import { Grid, Row, Col } from "react-bootstrap";
+
+const mapStateToProps = state => ({
+  appName: state.appName,
+  articles: state.articles
+});
+
+const mapDispatchToProps = dispatch => ({
+  onLoad: payload => dispatch({ type: "ARTICLE_HOME_PAGE_LOADED", payload })
+});
+
+class Article extends Component {
+  componentDidMount() {
+    this.props.onLoad(services.Articles.all());
+  }
+  render() {
+    return (
+      <div className="home-page">
+        <div className="container page">
+          <div className="row">
+            <MainView />
+          </div>
+          <div className="row">
+            <Grid>
+              <Row className="show-grid text-center">
+                <Col xs={12} sm={4} className="person-wrapper" />
+              </Row>
+            </Grid>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Article);
