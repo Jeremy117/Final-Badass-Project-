1a0b6ad265a37928ddc05975de6566d4abc9aa6a
Adam Klaasmeyer <adamklaasmeyer@gmail.com>
2018-04-04 11:00:18 -0700

    added date, time, favorites

--- client-app/src/components/App.js
+++ client-app/src/components/App.js
@@ -44,7 +44,7 @@ class App extends Component {
       services.setToken(token);
     }
     //if there is a token, we want to make an HTTP call for current user
-    //do we have that set up on the server side yet? idk
+    //do we have that set up on the server side yet?
     this.props.onLoad(token ? services.Auth.currentUser() : null, token);
   }
   render() {
@@ -71,36 +71,3 @@ class App extends Component {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
-
-// import React, { Component } from "react";
-// import "./App.css";
-// import { BrowserRouter as Router, Route } from "react-router-dom";
-// import Navbar from "./CustomNavbar";
-// import Register from "./Register";
-// import SignIn from "./SignIn";
-// import Dashboard from "./Dashboard";
-// import Home from "./Home";
-// import Article from "./Article";
-// import Profile from "./Profile";
-// import Footer from "./Footer";
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <Router>
-//         <div>
-//           <Navbar />
-//           <Footer />
-//           <Route exact path="/" component={Home} />
-//           <Route path="/SignIn" component={SignIn} />
-//           <Route path="/Register" component={Register} />
-//           <Route path="/Dashboard" component={Dashboard} />
-//           <Route path="/Article" component={Article} />
-//           <Route path="/Profile" component={Profile} />
-//         </div>
-//       </Router>
-//     );
-//   }
-// }
-
-// export default App;
--- client-app/src/components/ArticleListPreview.js
+++ client-app/src/components/ArticleListPreview.js
@@ -5,7 +5,7 @@ const ArticlePreview = ({ article }) => {
   // const article = props.article;
   return (
     <div className="article-preview">
-      {/* <div className="article-meta">
+      <div className="article-meta">
         <a>
           <img src={article.author.image} alt="avatar" />
         </a>
@@ -23,7 +23,7 @@ const ArticlePreview = ({ article }) => {
             {article.favoritesCount}
           </button>
         </div>
-      </div> */}
+      </div>
 
       <Link to={`article/${article.slug}`} className="preview-link">
         <h1>{article.title}</h1>
@@ -42,47 +42,3 @@ const ArticlePreview = ({ article }) => {
 };
 
 export default ArticlePreview;
-
-// import React from "react";
-
-// const ArticleListPreview = ({ article }) => {
-//   // const article = props.article;
-//   return (
-//     <div className="article-preview">
-//       <div className="article-meta">
-//         <a>
-//           <img src={article.author.image} alt="alt" />
-//         </a>
-
-//         <div className="info">
-//           <a className="author">{article.author.username}</a>
-//           <span className="date">
-//             {new Date(article.createdAt).toDateString()}
-//           </span>
-//         </div>
-
-//         <div className="pull-xs-right">
-//           <button className="btn btn-sm btn-outline-primary">
-//             <i className="ion-heart" />
-//             {article.favoritesCount}
-//           </button>
-//         </div>
-//       </div>
-
-//       <a to={`articles/${article.slug}`} className="preview-link">
-//         <h1>{article.title}</h1>
-//         <p>{article.description}</p>
-//         <span>Read more...</span>
-//         <ul className="tag-last">
-//           {article.tagList.map(tag => (
-//             <li className="tag-default tag-pill tag-outline" key={tag}>
-//               {tag}
-//             </li>
-//           ))}
-//         </ul>
-//       </a>
-//     </div>
-//   );
-// };
-
-// export default ArticleListPreview;
