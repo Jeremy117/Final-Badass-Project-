50fb3fc238250966e895dcae690f65ea585bd0ff
Jeremy117 <Blackflag117@yahoo.com>
2018-04-05 13:41:39 -0700

    Merge pull request #25 from Jeremy117/authorization-step11
    Authorization step11

--- client-app/src/components/Article/index.js
+++ client-app/src/components/Article/index.js
@@ -1,38 +1,90 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import MainView from "./MainView";
+import marked from "marked";
 import services from "../../services";
-
-import { Grid, Row, Col } from "react-bootstrap";
+import ArticleMeta from "./ArticleMeta";
+import CommentContainer from "./CommentContainer";
 
 const mapStateToProps = state => ({
-  appName: state.appName,
-  articles: state.articles
+  ...state.article,
+  currentUser: state.common.currentUser
 });
 
 const mapDispatchToProps = dispatch => ({
-  onLoad: payload => dispatch({ type: "ARTICLE_HOME_PAGE_LOADED", payload })
+  onLoad: payload => dispatch({ type: "ARTICLE_PAGE_LOADED", payload })
 });
 
 class Article extends Component {
   componentDidMount() {
-    this.props.onLoad(services.Articles.all());
+    /*
+    we'll need to get article by slug from the url
+    and the comments for this. 2 promises. ummmmm
+    */
+    const articleId = this.props.match.params.id;
+    this.props.onLoad(
+      Promise.all([
+        services.Articles.get(articleId),
+        services.Comments.forArticle(articleId)
+      ])
+    );
   }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.comments !== this.props.comments) {
+      console.log(nextProps);
+    }
+  }
+
+  /*
+marked is a library that compiles markdown into HTML - in order to get react to render raw HTML, we need to use this dangerouslySetInnerHTML property, because React sanitizes HTML by default.
+  */
   render() {
+    const article = this.props.article;
+    if (!article) {
+      return null;
+    }
+    const markup = { __html: marked(article.body) };
+    const canModify =
+      this.props.currentUser &&
+      this.props.currentUser.username === article.author.username;
     return (
-      <div className="home-page">
-        <div className="container page">
-          <div className="row">
-            <MainView />
-          </div>
-          <div className="row">
-            <Grid>
-              <Row className="show-grid text-center">
-                <Col xs={12} sm={4} className="person-wrapper" />
-              </Row>
-            </Grid>
+      <div className="article-page">
+        <div className="banner">
+          <div className="container">
+            <h1>{article.title}</h1>
+            <ArticleMeta article={this.props.article} canModify={canModify} />
           </div>
         </div>
+
+        <div className="container page">
+          <div className="row article-content">
+            <div className="col-xs-12">
+              <div dangerouslySetInnerHTML={markup} />
+
+              <ul className="tag-list">
+                {article.tagList.map(tag => {
+                  return (
+                    <li className="tag-default tag-pill tag-outline" key={tag}>
+                      {tag}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+
+          <hr />
+
+          <div className="article-actions" />
+        </div>
+
+        <div className="row" />
+        <CommentContainer
+          comments={this.props.comments || []}
+          errors={this.props.commentErrors}
+          slug={this.props.match.params.id}
+          currentUser={this.props.currentUser}
+        />
       </div>
     );
   }
--- routes/api/articles.js
+++ routes/api/articles.js
@@ -1,6 +1,24 @@
 const router = require("express").Router();
 const mongoose = require("mongoose");
 const Article = mongoose.model("Article");
+const User = mongoose.model("User");
+const Comment = mongoose.model("Comment");
+const auth = require("../auth");
+
+// Preload article objects on routes with ':article'
+router.param("article", function(req, res, next, slug) {
+  Article.findOne({ slug: slug })
+    .populate("author")
+    .then(function(article) {
+      if (!article) {
+        return res.sendStatus(404);
+      }
+      req.article = article;
+
+      return next();
+    })
+    .catch(next);
+});
 
 router.get("/", function(req, res, next) {
   //this is where we'd query db, give all the articles
@@ -8,7 +26,7 @@ router.get("/", function(req, res, next) {
   //and send them back
   //db.getCollection('articles').find({})
   Article.find()
-    .limit(5)
+    .limit(50)
     .exec()
     .then(results => {
       return res.json({
@@ -16,6 +34,144 @@ router.get("/", function(req, res, next) {
       });
     })
     .catch(next);
+});
+
+// return a article by slug-id, we also want to get user who is actually signed in.
+router.get("/:article", auth.optional, function(req, res, next) {
+  Promise.all([req.article.populate("author").execPopulate()])
+    .then(function(results) {
+      return res.json({ article: req.article.toJSONFor() });
+    })
+    .catch(next);
+});
+
+// return an article's comments
+router.get("/:article/comments", auth.optional, function(req, res, next) {
+  Promise.resolve(req.payload ? User.findById(req.payload.id) : null)
+    .then(function(user) {
+      return req.article
+        .populate({
+          path: "comments",
+          populate: {
+            path: "author"
+          },
+          options: {
+            sort: {
+              createdAt: "desc"
+            }
+          }
+        })
+        .execPopulate()
+        .then(function(article) {
+          return res.json({
+            comments: req.article.comments.map(function(comment) {
+              return comment.toJSONFor();
+            })
+          });
+        });
+    })
+    .catch(next);
+});
+
+// create a new Article,  auth required
+router.post("/", auth.required, function(req, res, next) {
+  User.findById(req.payload.id)
+    .then(function(user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      var article = new Article(req.body.article);
+
+      article.author = user;
+
+      return article.save().then(function() {
+        return res.json({ article: article.toJSONFor() });
+      });
+    })
+    .catch(next);
+});
+
+// update article
+router.put("/:article", auth.required, function(req, res, next) {
+  User.findById(req.payload.id).then(function(user) {
+    if (req.article.author._id.toString() === req.payload.id.toString()) {
+      if (typeof req.body.article.title !== "undefined") {
+        req.article.title = req.body.article.title;
+      }
+
+      if (typeof req.body.article.description !== "undefined") {
+        req.article.description = req.body.article.description;
+      }
+
+      if (typeof req.body.article.body !== "undefined") {
+        req.article.body = req.body.article.body;
+      }
+
+      if (typeof req.body.article.tagList !== "undefined") {
+        req.article.tagList = req.body.article.tagList;
+      }
+
+      req.article
+        .save()
+        .then(function(article) {
+          return res.json({ article: article.toJSONFor(user) });
+        })
+        .catch(next);
+    } else {
+      return res.sendStatus(403);
+    }
+  });
+});
+
+// delete article
+router.delete("/:article", auth.required, function(req, res, next) {
+  User.findById(req.payload.id)
+    .then(function(user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      if (req.article.author._id.toString() === req.payload.id.toString()) {
+        return req.article.remove().then(function() {
+          return res.sendStatus(204);
+        });
+      } else {
+        return res.sendStatus(403);
+      }
+    })
+    .catch(next);
+});
+
+// create a new comment
+router.post("/:article/comments", auth.required, function(req, res, next) {
+  User.findById(req.payload.id)
+    .then(function(user) {
+      if (!user) {
+        return res.sendStatus(401);
+      }
+
+      var comment = new Comment(req.body.comment);
+      comment.article = req.article;
+      comment.author = user;
+      /*
+      Save comment, on success, we want to add the newly created 
+      comment Object_id to the comments property
+      of that specific article, that is an array, so we use push
+      */
+      return comment.save().then(function() {
+        req.article.comments.push(comment);
+
+        /*
+        Once that comment Object_id is added to the article 
+        we need to save that.
+        */
+        return req.article.save().then(function(article) {
+          res.json({ comment: comment.toJSONFor() });
+        });
+      });
+    })
+    .catch(next); //--> if any of our saves/querys fail it triggers the catch
 });
 
 module.exports = router;
