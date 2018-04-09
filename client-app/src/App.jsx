ea99a4a9ff83ed06efae8e3819ce4aac659f9d27
alvaro cortes <frijoles4me@gmail.com>
2018-04-09 11:25:27 -0700

    updates

--- client-app/src/App.jsx
+++ client-app/src/App.jsx
@@ -6,19 +6,22 @@ import Home from "./components/Home";
 import Register from "./components/Register";
 import signUp from "./components/signUp";
 import Dashboard from "./components/Dashboard";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route path="/components/signUp" component={signUp} />
-          <Route path="/components/Register" component={Register} />
-          <Route path="/components/Dashboard" component={Dashboard} />
-        </div>
-      </Router>
+      <MuiThemeProvider>
+        <Router>
+          <div>
+            <Navbar />
+            <Route exact path="/" component={Home} />
+            <Route path="/components/signUp" component={signUp} />
+            <Route path="/components/Register" component={Register} />
+            <Route path="/components/Dashboard" component={Dashboard} />
+          </div>
+        </Router>
+      </MuiThemeProvider>
     );
   }
 }
--- client-app/src/components/Dashboard.jsx
+++ client-app/src/components/Dashboard.jsx
@@ -1,31 +1,15 @@
 import React, { Component } from "react";
 import "./Dashboard.css";
-
+import DatePicker from "material-ui/DatePicker";
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <div>
-                <div className="container2">
-                    <h1 /> Dashboard
-            </div>
-                <ul className="container1">
-                    <h2 />calendar
-              <h2 />newsfeed
-              <h2 />roster
-            </ul>
-                <ul className="container1">
-                    <div />calendar code
-              <div />newsfeed code
-              <div />roster code
-            </ul>
-            </div>
-
-        );
-
-    }
-
-
+  render() {
+    return (
+      <div>
+        <DatePicker hintText="Portrait Dialog" />
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
