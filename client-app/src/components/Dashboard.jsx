cee28d324aa900034e92b48b39126a0ed7ca4638
alvaro cortes <frijoles4me@gmail.com>
2018-04-10 15:54:26 -0700

    updates

--- client-app/src/components/Dashboard.jsx
+++ client-app/src/components/Dashboard.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
 import "./Dashboard.css";
 import DatePicker from "material-ui/DatePicker";
+import TextField from "material-ui/TextField";
 
 class Dashboard extends Component {
   render() {
     return (
       <div>
+        <TextField hintText="Hint Text" />
+        <br />
         <DatePicker hintText="Portrait Dialog" />
       </div>
     );
