06a9ae0f729ce5bc03b57076b0f0b8d920505614
Adam Klaasmeyer <adamklaasmeyer@gmail.com>
2018-04-18 11:44:30 -0700

    added static home page in index.js and home.css

--- client-app/src/components/Home/index.js
+++ client-app/src/components/Home/index.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import services from "../../services";
 import mainImage from "../../images/prototype-home-desktop-logo.jpg";
-// import "./home.css";
+import "./home.css";
 
 const mapStateToProps = state => ({
   appName: state.common.appName,
